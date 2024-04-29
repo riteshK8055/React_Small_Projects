1e6c6c36b0de911de0c99184f535dc75db3392ea
Ritesh Kumar <riteshk8055@gmail.com>
2024-04-29 11:32:09 +0530

    started 5th machine coding challenge

---
+++ quiz_app/src/App.jsx
@@ -0,0 +1,16 @@
+import React , { useState } from 'react';
+
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h1 className="bg-red-400">Hello</h1>
+      
+    </>
+  )
+}
+
+export default App
